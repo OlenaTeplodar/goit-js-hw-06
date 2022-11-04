b5f50e7de9201d909bc674a0ca4a0dc2c1c62c55
OlenaTeplodar <lena.golubenkova@gmail.com>
2022-11-04 12:44:04 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const refs = {
+  countDecrement: document.querySelector('[data-action="decrement"]'),
+    countIncrement: document.querySelector('[data-action="increment"]'),
+  countValue: document.querySelector('#value'),
+};
+
+refs.countDecrement.addEventListener('click', onCountDecrement);
+refs.countIncrement.addEventListener('click', onCountIncrement);
+
+let counterValue = 0;
+
+function onCountDecrement() {
+    refs.countValue.textContent = counterValue -= 1;
+}
+
+function onCountIncrement() {
+    refs.countValue.value = counterValue += 1;
+    refs.countValue.textContent = refs.countValue.value;
+}
