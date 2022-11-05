9d805c427e05b68e83efe40a913b2b658a8d952e
OlenaTeplodar <lena.golubenkova@gmail.com>
2022-11-05 11:59:47 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  colorChangeBtn: document.querySelector("button.change-color"),
+  spanColorInfo: document.querySelector('span.color'),
+};
+
+
+refs.colorChangeBtn.addEventListener('click', onColorChange);
+
+
+function onColorChange(event) {
+  refs.bodyColor.style.backgroundColor = `${getRandomHexColor(event)}`;
+  refs.spanColorInfo.textContent = getRandomHexColor(event);
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
